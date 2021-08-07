898954dbb1a2d315a3dc21b013eaf85c5d7794b4
Farshid Gholizadeh <66841257+f4r6d@users.noreply.github.com>
2021-08-07 19:20:31 +0430

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ db = SQL(uri)
 
 
 # db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, hash TEXT NOT NULL)")
-db.execute("ALTER TABLE users ADD COLUMN admin numeric")
+db.execute("ALTER TABLE users ADD COLUMN admin_user numeric")
 # db.execute("CREATE TABLE trans (user_id SERIAL , symbol TEXT NOT NULL, shares NUMERIC NOT NULL, price NUMERIC NOT NULL DEFAULT 0, ts  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(user_id) REFERENCES users(id))")
 
 # Make sure API key is set
